9c638e9e4eb8369f7c1ebc091249cc3dc7daa937
Thiago Fernandes Massa <thiagown@gmail.com>
2016-02-11 19:09:33 +0100

    Added lambda coins base

---
+++ src/cljs/haxlife/lambda_coins.cljs
@@ -0,0 +1,14 @@
+(ns haxlife.lambda-coins
+  (:require [haxlife.data.db :as db]))
+
+(def base-lambdas-per-second 1)
+(def base-lambdas-per-character 1)
+
+; Setting up
+(db/lambdas-setup base-lambdas-per-second base-lambdas-per-character)
+
+(defn next-second [per-second]
+  (.log js/console "NEXT SECOND PARAM:")
+  (.log js/console per-second)
+  ; add formula that computes addons
+  per-second)
